4e3cda4aee9500a90d3ca4e2bc3d591288e0718d
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-04 15:46:46 -0300

    Criar metodos classe funcionario

--- src/Exemplo2/Funcionario.java
+++ src/Exemplo2/Funcionario.java
@@ -1,4 +1,31 @@
 package Exemplo2;
 
 public class Funcionario extends Pessoa {
+    private String ctps;
+
+    public Funcionario() {
+
+    }
+
+    public Funcionario(String nome, String cpf, String ctps) {
+        super(nome, cpf);
+        this.ctps = ctps;
+    }
+
+    public String getCtps() {
+        return ctps;
+    }
+
+    public void setCtps(String ctps) {
+        this.ctps = ctps;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retornoFuncionario = new StringBuilder();
+        retornoFuncionario.append(super.toString());
+        retornoFuncionario.append("Carteira de Trabalho " + ctps);
+        return retornoFuncionario.toString();
+    }
 }
+
